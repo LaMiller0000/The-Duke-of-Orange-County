1f050432cf9fb97b2abae35af4f770921f4fe7d7
LaMiller0000 <160054780+LaMiller0000@users.noreply.github.com>
2025-04-10 14:09:37 -0400

    targeting

--- Project/Scripts/TestSpringArm.gd
+++ Project/Scripts/TestSpringArm.gd
@@ -2,9 +2,11 @@ extends Node3D
 
 func _ready() -> void:
 	set_as_top_level(true)
+	
 
 
 func _unhandled_input(event : InputEvent) -> void:
+	"""
 	if Input.is_action_pressed("ui_left"):
 		rotation_degrees.y += 10
 	elif Input.is_action_pressed("ui_right"):
@@ -17,9 +19,9 @@ func _unhandled_input(event : InputEvent) -> void:
 		#rotation_degrees.x -= event.relative.y * mouse_sensitivity
 		#rotation_degrees.x = clamp(rotation_degrees.x, -90.0, 30.0)
 		
-		rotation_degrees.y -= event.relative.x * mouse_sensitivity
+		rotation_degrees.y -= event.relative.x * 1.2
 		rotation_degrees.y = wrapf(rotation_degrees.y, 0.0, 360.0)
-		"""
+
 	"""
 func _process(delta):
 	if InputEventJoypadMotion:
