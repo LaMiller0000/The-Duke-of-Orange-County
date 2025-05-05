6efaa7b18351e8552fb41cbdabadde825214c137
LaMiller0000 <160054780+LaMiller0000@users.noreply.github.com>
2025-05-04 23:46:27 -0400

    little formatting change

--- Project/Test/Test Scenes/TestDialogRoot.cs
+++ Project/Test/Test Scenes/TestDialogRoot.cs
@@ -3,18 +3,21 @@ using System;
 
 public partial class TestDialogRoot : Node2D
 {
-	[Export] public string[] txt = { "I don't know why I even try anymore",
-									 "There's this burning...",
-									 "something, in me.",
-									 "To be honest, how can this all happen?",
-									 "We were all fine, all alright in our ways.",
-									 "But no!",
-									 "It changed, it always changes.",
-									 "I was completely, and utterly fine in this state.",
-									 "It changed, and everything's worse.",
-									 "There was no reason for it to change,",
-									 "but it did.",
-									 "It really did." };
+	[Export] public string[] txt = 
+	{
+		"I don't know why I even try anymore",
+		"There's this burning...", 
+		"something, in me.",
+		"To be honest, how can this all happen?",
+		"We were all fine, all alright in our ways.",
+		"But no!",
+		"It changed, it always changes.",
+		"I was completely, and utterly fine in this state.",
+		"It changed, and everything's worse.",
+		"There was no reason for it to change,",
+		"but it did.",
+		"It really did."
+	};
 	private TestDialog _testDialog;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
